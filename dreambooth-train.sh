d9a06576f36b7d2448591f5b938e08930d29af07
Berk Elmas <42638691+berkelmas@users.noreply.github.com>
2023-06-02 17:55:46 +0300

    Update dreambooth-train.sh

--- dreambooth-train.sh
+++ dreambooth-train.sh
@@ -30,12 +30,12 @@ python train_dreambooth.py \
 --with_prior_preservation --prior_loss_weight=1.0 \
 --instance_prompt="${2}" \
 --class_prompt="${1}" \
---resolution=768  \
+--resolution=512  \
 --train_batch_size=1  \
 --sample_batch_size=1 \
 --gradient_accumulation_steps=2 --gradient_checkpointing  \
 --use_8bit_adam  \
---learning_rate=5e-6  \
+--learning_rate=1e-6  \
 --lr_scheduler="constant"  \
 --lr_warmup_steps=0  \
 --num_class_images=${IMAGES} \
